8b2fd8a986f3cd397c7d89f1d5f873607f432049
Matthew Wilding <mbwilding@gmail.com>
2023-11-08 22:41:52 +0800

    Clean up of hook code

--- windows/src/init.rs
+++ windows/src/init.rs
@@ -8,22 +8,17 @@ use windows::{
         System::LibraryLoader::GetModuleHandleW,
         UI::{
             Accessibility::SetWinEventHook,
-            WindowsAndMessaging::{
-                DispatchMessageW, GetMessageW, TranslateMessage, EVENT_OBJECT_FOCUS, MSG,
-                WINEVENT_OUTOFCONTEXT,
-            },
+            WindowsAndMessaging::{GetMessageW, EVENT_OBJECT_FOCUS, MSG, WINEVENT_OUTOFCONTEXT},
         },
     },
 };
 
 pub fn start() -> Result<()> {
     unsafe {
-        let h_instance = GetModuleHandleW(PCWSTR::null())?;
-
-        let _event_hook = SetWinEventHook(
+        let event_hook = SetWinEventHook(
             EVENT_OBJECT_FOCUS,
             EVENT_OBJECT_FOCUS,
-            h_instance,
+            GetModuleHandleW(PCWSTR::null())?,
             Some(get_focused_window_details),
             0,
             0,
@@ -36,10 +31,7 @@ pub fn start() -> Result<()> {
 
         loop {
             // Will wait here for a message, so no sleep is needed in the loop
-            while GetMessageW(&mut msg, HWND(0), 0, 0).as_bool() {
-                TranslateMessage(&msg);
-                DispatchMessageW(&msg);
-            }
+            while GetMessageW(&mut msg, HWND(0), 0, 0).as_bool() {}
         }
     }
 }
