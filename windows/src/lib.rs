da4d3914fc053eb94cf2363c0bf2bf89e74c7a3a
Matthew Wilding <mbwilding@gmail.com>
2023-11-08 22:58:51 +0800

    Cleaned windows crate

--- windows/src/lib.rs
+++ windows/src/lib.rs
@@ -1,4 +1,3 @@
 pub mod init;
-pub mod layer;
 pub mod message_loop;
 pub mod window_data;
--- windows/src/window_data.rs
+++ windows/src/window_data.rs
@@ -1,14 +1,23 @@
+use common::app::App;
 use std::path::Path;
 use tracing::{error, trace};
-use windows::Win32::System::Threading::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
-use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextW, GetWindowThreadProcessId};
 use windows::Win32::{
     Foundation::{HWND, MAX_PATH},
-    System::{ProcessStatus::K32GetModuleFileNameExW, Threading::OpenProcess},
-    UI::WindowsAndMessaging::GetWindowTextLengthW,
+    System::{
+        ProcessStatus::K32GetModuleFileNameExW,
+        Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
+    },
+    UI::WindowsAndMessaging::{GetWindowTextLengthW, GetWindowTextW, GetWindowThreadProcessId},
 };
 
-pub unsafe fn get_exe_name(window_handle: HWND) -> Option<String> {
+pub unsafe fn hydrate(window_handle: HWND) -> App {
+    App {
+        window_title: get_window_title(window_handle),
+        exe_name: get_exe_name(window_handle),
+    }
+}
+
+unsafe fn get_exe_name(window_handle: HWND) -> Option<String> {
     let mut process_id: u32 = 0;
 
     let thread_id = GetWindowThreadProcessId(window_handle, Some(&mut process_id as *mut u32));
@@ -58,7 +67,7 @@ pub unsafe fn get_exe_name(window_handle: HWND) -> Option<String> {
     Some(exe_name)
 }
 
-pub unsafe fn get_window_title(h_wnd: HWND) -> Option<String> {
+unsafe fn get_window_title(h_wnd: HWND) -> Option<String> {
     let title_length = GetWindowTextLengthW(h_wnd) + 1;
     let mut window_title: Vec<u16> = vec![0; title_length as usize];
     let _ = GetWindowTextW(h_wnd, window_title.as_mut_slice());
