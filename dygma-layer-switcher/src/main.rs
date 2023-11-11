0e9c67e6b24eb71aaf1517bd3ae2ad794785ba42
Matthew Wilding <mbwilding@gmail.com>
2023-11-11 16:03:26 +0800

    Added some comments

--- dygma-layer-switcher/src/main.rs
+++ dygma-layer-switcher/src/main.rs
@@ -12,8 +12,10 @@ windows_service::define_windows_service!(ffi_service_main, windows_service_main)
 
 fn main() -> anyhow::Result<()> {
     #[cfg(windows)]
+    // This  will fail fast if it is not being launched as a service and execution will continue
     if windows_service::service_dispatcher::start("Dygma Layer Switcher", ffi_service_main).is_ok()
     {
+        // Service has finished, we've already ran the logic below internally, so we can just return
         return Ok(());
     };
 
