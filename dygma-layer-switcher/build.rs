e7edb782775afae0e237a38670d28929f0d66147
Matthew Wilding <mbwilding@gmail.com>
2023-11-13 11:01:04 +0800

    Added no-admin feature as running as admin is default. Admin allows us to get the exe_name of windows running in elevated user-space.

--- dygma-layer-switcher/build.rs
+++ dygma-layer-switcher/build.rs
@@ -3,8 +3,8 @@ extern crate embed_resource;
 fn main() {
     embed_resource::compile("../assets/windows/tray.rc", embed_resource::NONE);
 
-    // If debug mode, don't require admin. Admin allows us to get the exe_name of windows running outside of non-elevated user-space.
-    if !cfg!(debug_assertions) {
+    // Admin allows us to get the exe_name of windows running in elevated user-space.
+    if !cfg!(feature = "no-admin") && !cfg!(debug_assertions) {
         embed_resource::compile("../assets/windows/manifest.rc", embed_resource::NONE);
     }
 }
