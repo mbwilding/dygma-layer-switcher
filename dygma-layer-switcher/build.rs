0d430970b37c7f4e74841bdbc3b9fc0e58023d0e
Matthew Wilding <mbwilding@gmail.com>
2023-11-14 13:23:16 +0800

    Testing fix for embedded resources on Windows

--- dygma-layer-switcher/build.rs
+++ dygma-layer-switcher/build.rs
@@ -1,12 +1,10 @@
 extern crate embed_resource;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        embed_resource::compile("../assets/windows/tray.rc", embed_resource::NONE);
+    embed_resource::compile("../assets/windows/tray.rc", embed_resource::NONE);
 
-        // Admin allows us to get the exe_name of windows running in elevated user-space.
-        if !cfg!(feature = "no-admin") && !cfg!(debug_assertions) {
-            embed_resource::compile("../assets/windows/manifest.rc", embed_resource::NONE);
-        }
+    // Admin allows us to get the exe_name of windows running in elevated user-space.
+    if !cfg!(feature = "no-admin") && !cfg!(debug_assertions) {
+        embed_resource::compile("../assets/windows/manifest.rc", embed_resource::NONE);
     }
 }
