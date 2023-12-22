55b3a1b4d9f6c14faea86cb3fb61a2b9a4d998fd
Matthew Wilding <mbwilding@gmail.com>
2023-12-22 18:11:25 +0800

    Fixed icon

--- dygma-layer-switcher/build.rs
+++ dygma-layer-switcher/build.rs
@@ -1,6 +1,8 @@
 extern crate embed_resource;
 
 fn main() {
+    embed_resource::compile("../assets/windows/icon.rc", embed_resource::NONE);
+
     // Admin allows us to get the process of windows running in elevated user-space.
     if !cfg!(feature = "no-admin") && !cfg!(debug_assertions) {
         embed_resource::compile("../assets/windows/manifest.rc", embed_resource::NONE);
