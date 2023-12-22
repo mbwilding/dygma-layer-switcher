91b50d0fa54f2f1b658db91eab0452e24332730c
Matthew Wilding <mbwilding@gmail.com>
2023-12-22 18:08:07 +0800

    Removed more tray stuff

--- dygma-layer-switcher/build.rs
+++ dygma-layer-switcher/build.rs
@@ -1,8 +1,6 @@
 extern crate embed_resource;
 
 fn main() {
-    embed_resource::compile("../assets/windows/tray.rc", embed_resource::NONE);
-
     // Admin allows us to get the process of windows running in elevated user-space.
     if !cfg!(feature = "no-admin") && !cfg!(debug_assertions) {
         embed_resource::compile("../assets/windows/manifest.rc", embed_resource::NONE);
