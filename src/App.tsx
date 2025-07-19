8bf6400c027a1944e23c3479996278f8c182f21c
Matthew Wilding <mbwilding@gmail.com>
2025-07-19 20:29:30 +0800

    Layer test

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,23 @@ function App() {
     focus.connect_first_available();
   }, []);
 
+  function renderLayerButtons() {
+    const buttons = [];
+    for (let i = 0; i < 10; i++) {
+      buttons.push(
+        <button key={i} onClick={() => focus.layer_activate(i)}>
+          {i + 1}
+        </button>,
+      );
+    }
+    return buttons;
+  }
+
   return (
     <main className="container">
       <h1>Welcome to Dygma Layer Switcher</h1>
 
-      <div className="row">
-        {Array.from({ length: 10 }, (_, i) => (
-          <button key={i} onClick={() => focus.layer_activate(i)}>
-            {i}
-          </button>
-        ))}
-      </div>
+      <div className="row">{renderLayerButtons()}</div>
     </main>
   );
 }
